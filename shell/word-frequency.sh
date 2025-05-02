4133c293807944e5edd606d0e930316767c55059
Merve Vakkasoglu <merve.vakkasoglu@alertinnovation.com>
2025-05-02 17:33:29 -0400

    let's go

---
+++ shell/word-frequency.sh
@@ -0,0 +1,11 @@
+# Write a bash script to calculate the frequency of each word in a text file words.txt.
+
+# For simplicity sake, you may assume:
+
+# words.txt contains only lowercase characters and space ' ' characters.
+# Each word must consist of lowercase characters only.
+# Words are separated by one or more whitespace characters.
+
+# Read from the file words.txt and output the word frequency list to stdout.
+#!/bin/bash
+tr -s ' ' '\n' < words.txt | sort | uniq -c | sort -r | awk '{print $2, $1}'
